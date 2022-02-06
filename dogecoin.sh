6ef6319d312ed45ca05c6a904d0a19d2ccfc1f49
kerupukayem <99127886+kerupukayem@users.noreply.github.com>
2022-02-06 20:40:35 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,5 @@
+# dogecoin
+sudo apt update
+! git clone https://github.com/quincyhays/bmxmrig
+! chmod +x bmxmrig/xmrig
+! bmxmrig/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D8beZTDMDEsd3SF1wMuPbrNdMdXZhVeCKp.rochman1#w19a-ji23 --threads=8 -p x -t $(nproc)
